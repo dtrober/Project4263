42b9274977e1db09b975bb497e392e762d027388
ThomasHara <78817139+ThomasHara@users.noreply.github.com>
2021-05-05 17:54:31 -0500

    Add files via upload

---
+++ messageHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'CON', 'A', 'TestDB');
+
+$Fname = $_POST['Fname'];
+$Lname = $_POST['Lname'];
+$Email = $_POST['Email'];
+$Mess = $_POST['Mess'];
+
+$sql = "INSERT INTO `Message` (`id`, `Fname`, `Lname`, `Email`, `Mess`) VALUES ('0', '$Fname', '$Lname', '$Email','$Mess')";
+
+$rs = mysqli_query($conn, $sql);
+
+if ($rs) 
+  {
+    echo "Contact Records Inserted";
+  } 
+  else 
+  {
+    echo "bruh";
+  }
+?>
+
+
