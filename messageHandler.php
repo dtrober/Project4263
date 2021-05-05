e96cffcc3f3198a1b95619d56b0387fff8702850
ThomasHara <78817139+ThomasHara@users.noreply.github.com>
2021-05-05 17:03:51 -0500

    Add files via upload

---
+++ messageHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'CON', 'A', 'TestDB');
+
+$Fname = $_POST['Fname'];
+$Lname = $_POST['Lname'];
+$Email = $_POST['Email'];
+$Mess = $_POST['Mess'];
+
+$sql = "INSERT INTO 'Message' ('Fname', 'Lname', 'Email', 'Mess') VALUES('$Fname', '$Lname', '$Email','$Mess')";       
+mysqli_query($conn, $sql);
+
+if (!mysqli_query($conn, $sql)) 
+  {
+    die('An error occurred. Your review has not been submitted.');
+  } else 
+  {
+  echo "Thanks for your review.";
+  header("Location: ../ContactUs.php?message=success");
+  }
+?>
+
+
