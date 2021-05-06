59678f6fcf5acf823c09ae298cf5651a1a0543a9
ThomasHara <78817139+ThomasHara@users.noreply.github.com>
2021-05-05 19:32:54 -0500

    Delete dbh.inc.php

--- includes/dbh.inc.php
+++
@@ -1,9 +0,0 @@
-<!-- This file establishes the connection to the database -->
-<?php
-
-$dbServername = "localhost";
-$dbUsername = "CON";
-$dbPassword = "A";
-$dbName = "TestDB";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
